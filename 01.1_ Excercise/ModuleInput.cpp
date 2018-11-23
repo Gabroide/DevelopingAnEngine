3c6d469beaf9c4d26c05c2d29bc991f958044a45
_gaspode <gaspode@uoc.edu>
2018-11-23 18:31:58 +0100

    TODO 1 performaed. The window closes when ESC is pushed

--- 01.1_ Excercise/ModuleInput.cpp
+++ 01.1_ Excercise/ModuleInput.cpp
@@ -1,14 +1,21 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleInput.h"
+
 #include "SDL/include/SDL.h"
 
+#define MAX_KEYS 300
+
 ModuleInput::ModuleInput()
-{}
+{
+	keyboard = new KeyState[MAX_KEYS];
+}
 
 // Destructor
 ModuleInput::~ModuleInput()
-{}
+{
+	
+}
 
 // Called before render is available
 bool ModuleInput::Init()
@@ -29,13 +36,85 @@ bool ModuleInput::Init()
 // Called every draw update
 update_status ModuleInput::Update()
 {
-	SDL_PumpEvents();
-
-	keyboard = SDL_GetKeyboardState(NULL);
+	
+	//SDL_PumpEvents();
+	//keyboard = SDL_GetKeyboardState(NULL);
 
 	// TODO 1: Make the application properly close when ESC is pressed (do not use exit())
 
+	/* CODE ADDED */
+	static SDL_Event event;
+
+	const Uint8* keys = SDL_GetKeyboardState(NULL);
+
+	for (int i = 0; i < MAX_KEYS; ++i)
+	{
+		if (keys[i] == 1)
+		{
+			if (keyboard[i] == KEY_IDLE)
+				keyboard[i] = KEY_DOWN;
+			else
+				keyboard[i] = KEY_REPEAT;
+		}
+		else
+		{
+			if (keyboard[i] == KEY_REPEAT || keyboard[i] == KEY_DOWN)
+				keyboard[i] = KEY_UP;
+			else
+				keyboard[i] = KEY_IDLE;
+		}
+	}
+
 	// Homework: Make the application close up when pressing “X” button of the window
+	
+	/*while (SDL_PollEvent(&event) != 0)
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			windowEvents[WE_QUIT] = true;
+			break;
+
+		case SDL_WINDOWEVENT:
+			switch (event.window.event)
+			{
+				//case SDL_WINDOWEVENT_LEAVE:
+			case SDL_WINDOWEVENT_HIDDEN:
+			case SDL_WINDOWEVENT_MINIMIZED:
+			case SDL_WINDOWEVENT_FOCUS_LOST:
+				windowEvents[WE_HIDE] = true;
+				break;
+
+				//case SDL_WINDOWEVENT_ENTER:
+			case SDL_WINDOWEVENT_SHOWN:
+			case SDL_WINDOWEVENT_FOCUS_GAINED:
+			case SDL_WINDOWEVENT_MAXIMIZED:
+			case SDL_WINDOWEVENT_RESTORED:
+				windowEvents[WE_SHOW] = true;
+				break;
+			}
+			break;
+/*
+		case SDL_MOUSEBUTTONDOWN:
+			mouse_buttons[event.button.button - 1] = KEY_DOWN;
+			break;
+
+		case SDL_MOUSEBUTTONUP:
+			mouse_buttons[event.button.button - 1] = KEY_UP;
+			break;
+
+		case SDL_MOUSEMOTION:
+			mouse_motion.x = event.motion.xrel / SCREEN_SIZE;
+			mouse_motion.y = event.motion.yrel / SCREEN_SIZE;
+			mouse.x = event.motion.x / SCREEN_SIZE;
+			mouse.y = event.motion.y / SCREEN_SIZE;
+			break;*/
+		/*}
+	}*/
+
+	//if (GetWindowEvent(EventWindow::WE_QUIT) == true || GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+	if (GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		return UPDATE_STOP;
 
 	return UPDATE_CONTINUE;
 }
@@ -47,3 +126,9 @@ bool ModuleInput::CleanUp()
 	SDL_QuitSubSystem(SDL_INIT_EVENTS);
 	return true;
 }
+
+// ---------
+/*bool ModuleInput::GetWindowEvent(EventWindow ev) const
+{
+	return windowEvents[ev];
+}*/
--- 01.1_ Excercise/ModuleInput.h
+++ 01.1_ Excercise/ModuleInput.h
@@ -4,7 +4,25 @@
 #include "Module.h"
 #include "Globals.h"
 
+#include "SDL/include/SDL_scancode.h"
+
 typedef unsigned __int8 Uint8;
+
+/*enum EventWindow
+{
+	WE_QUIT = 0,
+	WE_HIDE = 1,
+	WE_SHOW = 2,
+	WE_COUNT
+};*/
+
+enum KeyState
+{
+	KEY_IDLE = 0,
+	KEY_DOWN,
+	KEY_REPEAT,
+	KEY_UP
+};
 
 class ModuleInput : public Module
 {
@@ -15,10 +33,21 @@ public:
 	bool Init() override;
 	bool CleanUp() override;
 
+	// Check for window events last frame
+	//bool GetWindowEvent(EventWindow code) const;
+
 	update_status Update() override;
 
+	// Check key states (includes mouse and joy buttons)
+	KeyState GetKey(int id) const
+	{
+		return keyboard[id];
+	}
+
 private:
-	const Uint8 *keyboard = NULL;
+	//bool		windowEvents[WE_COUNT];
+	KeyState*	keyboard;
+	//const Uint8 *keyboard = NULL;
 };
 
 #endif // __ModuleInput_h__
