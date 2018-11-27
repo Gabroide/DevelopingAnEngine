bd0ae14e3818440feb25d351e4f4fe680c2d59d6
_gaspode <gaspode@uoc.edu>
2018-11-28 00:00:35 +0100

    Esternal libraries added to the project

---
+++ 02_Cap02/ModuleScene.cpp
@@ -0,0 +1,24 @@
+#include "ModuleScene.h"
+#include "Globals.h"
+#include "Application.h"
+
+
+ModuleScene::ModuleScene()
+{
+}
+
+
+ModuleScene::~ModuleScene()
+{
+}
+
+bool ModuleScene::Start()
+{
+	return true;
+}
+
+update_status ModuleScene::Update()
+{
+	return UPDATE_CONTINUE;
+
+}
