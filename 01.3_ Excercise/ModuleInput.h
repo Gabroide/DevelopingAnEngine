08459e24b0bdcad466b497c465ca2e44ec7a5e57
_gaspode <gaspode@uoc.edu>
2018-11-27 10:58:36 +0100

    Start() performed and added to all modules

---
+++ 01.3_ Excercise/ModuleInput.h
@@ -0,0 +1,54 @@
+#ifndef __ModuleInput_h__
+#define __ModuleInput_h__
+
+#include "Module.h"
+#include "Globals.h"
+
+#include "SDL/include/SDL_scancode.h"
+
+typedef unsigned __int8 Uint8;
+
+enum EventWindow
+{
+	WE_QUIT = 0,
+	WE_HIDE = 1,
+	WE_SHOW = 2,
+	WE_COUNT
+};
+
+enum KeyState
+{
+	KEY_IDLE = 0,
+	KEY_DOWN,
+	KEY_REPEAT,
+	KEY_UP
+};
+
+class ModuleInput : public Module
+{
+public:
+	ModuleInput();
+	~ModuleInput();
+
+	bool Init() override;
+	bool CleanUp() override;
+	bool Start() override;
+
+	// Check for window events last frame
+	bool GetWindowEvent(EventWindow code) const;
+
+	update_status Update() override;
+
+	// Check key states (includes mouse and joy buttons)
+	KeyState GetKey(int id) const
+	{
+		return keyboard[id];
+	}
+
+private:
+	bool		windowEvents[WE_COUNT];
+	KeyState*	keyboard;
+	//const Uint8 *keyboard = NULL;
+};
+
+#endif // __ModuleInput_h__
---
+++ 01.3_ Excercise/ModuleWindow.h
@@ -0,0 +1,26 @@
+#ifndef __ModuleWindow_h__
+#define __ModuleWindow_h__
+
+#include "Module.h"
+#include "SDL/include/SDL.h"
+
+class Application;
+
+class ModuleWindow : public Module
+{
+public:
+	ModuleWindow();
+
+	// Destructor
+	virtual ~ModuleWindow();
+
+	bool Init() override; // Called before quitting
+	bool CleanUp() override; // Called before quitting
+	bool Start() override;
+
+public:
+	SDL_Window* window = NULL; //The window we'll be rendering to
+	SDL_Surface* screen_surface = NULL; //The surface contained by the window
+};
+
+#endif // __ModuleWindow_h__
