e797f432f7e85a6558448c1a6ce14ae3f3d13b9e
_gaspode <gaspode@uoc.edu>
2018-12-01 12:16:50 +0100

    Bug in ModuleCamera repaired

--- 04_Cap04/ModuleCamera.h
+++ 04_Cap04/ModuleCamera.h
@@ -45,6 +45,26 @@ public:
 
 public:
 
+	Frustum frustum;
+	math::float3& cameraPosition = math::float3(0.0f, 1.0f, 10.0f);
+	math::float3& cameraFront = math::float3(0.0f, 0.0f, -1.0f);
+	math::float3& cameraUp = math::float3(0.0f, 1.0f, 0.0f);
+
+	float mSpeed = 0.5f;
+	float rSpeed = 1.0f;
+
+	float pitch = 0;
+	float yaw = 0;
+
+	bool firstMouse = true;
+	int lastX = 0;
+	int lastY = 0;
+
+	float fovY = 45.0f;
+	float fovX = 45.0f;
+
+	int screenWidth = SCREEN_WIDTH;
+	int screenHeight = SCREEN_HEIGHT;
 };
 
 #endif // __ModuleCamera_h__
