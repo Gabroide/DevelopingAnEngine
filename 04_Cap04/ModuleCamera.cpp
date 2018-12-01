a6f8c8153865d4364bb1c9bb0258fe8d92142266
_gaspode <gaspode@uoc.edu>
2018-12-01 12:05:13 +0100

    Camera class performed

--- 04_Cap04/ModuleCamera.cpp
+++ 04_Cap04/ModuleCamera.cpp
@@ -47,3 +47,145 @@ math::float4x4 ModuleCamera::LookAt(const math::float3& cameraPosition, math::fl
 	return matrix;
 }
 
+void ModuleCamera::Focus()
+{
+	math::AABB boundingBox = math::AABB(App->modelLoader->minPoint, App->modelLoader->maxPoint);
+	math::float3 center = boundingBox.FaceCenterPoint(5);
+
+	//Reset all variables (position, front, up, fov, pitch, yaw, firstmouse)
+	frustum.pos = cameraPosition = math::float3(center.x, center.y, 0.0f);
+	frustum.front = cameraFront = math::float3(0.0f, 0.0f, -1.0f);
+	frustum.up = cameraUp = math::float3(0.0f, 1.0f, 0.0f);
+	frustum.verticalFov = math::pi / 4.0f;
+	frustum.horizontalFov = 2.f * atanf(tanf(frustum.verticalFov * 0.5f) * ((float)screenWidth / (float)screenHeight));
+	fovY = 45.0f;
+	fovX = 45.0f;
+	pitch = 0;
+	yaw = 0;
+	firstMouse = true;
+
+	//Add distance still we can see all the bounding box
+	while (!App->camera->frustum.Contains(boundingBox))
+	{
+		App->camera->cameraPosition.z = App->camera->frustum.pos.z += 10;
+	}
+}
+
+void ModuleCamera::Move(const Directions dir)
+{
+	switch (dir) {
+	case UP:
+		frustum.pos = cameraPosition += cameraUp.Normalized() * mSpeed;
+		break;
+	case DOWN:
+		frustum.pos = cameraPosition -= cameraUp.Normalized() * mSpeed;
+		break;
+	case FORWARD:
+		frustum.pos = cameraPosition += cameraFront.Normalized() * mSpeed;
+		break;
+	case BACKWARD:
+		frustum.pos = cameraPosition -= cameraFront.Normalized() * mSpeed;
+		break;
+	case LEFT:
+		frustum.pos = cameraPosition += cameraUp.Cross(cameraFront).Normalized() * mSpeed;
+		break;
+	case RIGHT:
+		frustum.pos = cameraPosition -= cameraUp.Cross(cameraFront).Normalized() * mSpeed;
+		break;
+	}
+}
+
+void ModuleCamera::SetPlaneDistances(const float nearDist, const float farDist)
+{
+	if (nearDist > 0.0f && nearDist < frustum.farPlaneDistance)
+	{
+		frustum.nearPlaneDistance = nearDist;
+	}
+
+	if (farDist > 0.0f && farDist > frustum.nearPlaneDistance)
+	{
+		frustum.farPlaneDistance = farDist;
+	}
+}
+
+void ModuleCamera::WindowResized(const unsigned width, const unsigned height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+	SetHorizontalFOV(fovX);
+	SetVerticalFOV(fovY);
+}
+
+void ModuleCamera::SetHorizontalFOV(const float fovX) {
+	frustum.horizontalFov = math::DegToRad(fovX);
+	frustum.verticalFov = 2.0f * atanf(tanf(frustum.horizontalFov * 0.5f) * ((float)screenHeight / (float)screenWidth));
+}
+
+void ModuleCamera::SetVerticalFOV(const float fovY) {
+	frustum.verticalFov = math::DegToRad(fovY);
+	frustum.horizontalFov = 2.0f * atanf(tanf(frustum.verticalFov * 0.5f) * ((float)screenWidth / (float)screenHeight));
+}
+
+void ModuleCamera::RefenceGround() const
+{
+	glLineWidth(1.0f);
+	float d = 200.0f;
+	float color[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glUniform4fv(glGetUniformLocation(App->program->axisProgram, "newColor"), 1, color);
+	glBegin(GL_LINES);
+	for (float i = -d; i <= d; i += 1.0f)
+	{
+		glVertex3f(i, 0.0f, -d);
+		glVertex3f(i, 0.0f, d);
+		glVertex3f(-d, 0.0f, i);
+		glVertex3f(d, 0.0f, i);
+	}
+	glEnd();
+}
+
+void ModuleCamera::ReferenceAxis() const
+{
+	glLineWidth(2.0f);
+
+	// red X
+	float red[4] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	glUniform4fv(glGetUniformLocation(App->program->axisProgram, "newColor"), 1, red);
+
+	glBegin(GL_LINES);
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.1f, 0.0f); glVertex3f(1.1f, -0.1f, 0.0f);
+	glVertex3f(1.1f, 0.1f, 0.0f); glVertex3f(1.0f, -0.1f, 0.0f);
+	glEnd();
+
+	// green Y
+	float green[4] = { 0.0f, 1.0f, 0.0f, 1.0f };
+	glUniform4fv(glGetUniformLocation(App->program->axisProgram, "newColor"), 1, green);
+
+	glBegin(GL_LINES);
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.0f, 1.15f, 0.0f); glVertex3f(0.0f, 1.05f, 0.0f);
+	glEnd();
+
+	// blue Z
+	float blue[4] = { 0.0f, 0.0f, 1.0f, 1.0f };
+	glUniform4fv(glGetUniformLocation(App->program->axisProgram, "newColor"), 1, blue);
+
+	glBegin(GL_LINES);
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.05f, 0.1f, 1.05f); glVertex3f(0.05f, 0.1f, 1.05f);
+	glVertex3f(0.05f, 0.1f, 1.05f); glVertex3f(-0.05f, -0.1f, 1.05f);
+	glVertex3f(-0.05f, -0.1f, 1.05f); glVertex3f(0.05f, -0.1f, 1.05f);
+	glEnd();
+
+	glLineWidth(1.0f);
+}
+
+bool ModuleCamera::CleanUp()
+{
+	return true;
+}
--- 04_Cap04/ModuleCamera.h
+++ 04_Cap04/ModuleCamera.h
@@ -1,8 +1,50 @@
-#pragma once
-class ModuleCamera
+#ifndef __ModuleCamera_h__
+#define __ModuleCamera_h__
+
+#include "Module.h"
+#include "MathGeoLib.h"
+#include "Point.h"
+#include "glew-2.1.0\include\GL\glew.h"
+
+enum Directions {
+	UP,
+	DOWN,
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+
+
+class ModuleCamera : public Module
 {
 public:
 	ModuleCamera();
 	~ModuleCamera();
+
+	bool Init() override;
+	bool CleanUp() override;
+
+	update_status PreUpdate() override;
+	update_status Update() override;
+
+	void SetPlaneDistances(const float nearDist, const float farDist);
+	math::float4x4 LookAt(const math::float3& cameraPosition, math::float3& cameraFront, const math::float3& cameraUp);
+	void Move(const Directions dir);
+	void MouseUpdate();
+	void SetVerticalFOV(const float fovY);
+	void SetHorizontalFOV(const float fovX);
+	void Orbit();
+	void Focus();
+
+	void RefenceGround() const;
+	void ReferenceAxis() const;
+
+	void WindowResized(const unsigned width, const unsigned height);
+
+public:
+
 };
 
+#endif // __ModuleCamera_h__
